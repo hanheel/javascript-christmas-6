766e2cbc92a22328b621231e4a622192b55c0ead
hanheel <hanhee630630@gmail.com>
2024-12-14 00:59:23 +0900

    fix(InputView) : return 값이 없는 오류를 해결한다 기존) date가 while문 안에 지역변수로 존재 변경 후) 전역변수로 바꾸어 return시 포함되도록 변경

--- src/InputView.js
+++ src/InputView.js
@@ -5,8 +5,9 @@ const InputView = {
   async readDate() {
     Console.print("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
     let isValidate = false;
+    let date = 0;
     while (!isValidate) {
-      const date = await Console.readLineAsync(
+      date = await Console.readLineAsync(
         "12월 중 식당 예상 방문 날짜는 언제인가요?"
       );
       isValidate = Validate.date(date);
